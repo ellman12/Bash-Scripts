b8476716ffe5b08c92f90c25ae2d26829ec9331c
Elliott <ellduc4@gmail.com>
2023-12-26 20:02:40 -0600

    Fix 2 functions using () instead of "" for parameters

--- Bash Scripts/ffmpeg.sh
+++ Bash Scripts/ffmpeg.sh
@@ -35,8 +35,8 @@ compress_video() {
 
 	inputFile="$1"
 	outputFile="$2"
-	width=($3)
-	height=($4)
+	width="$3"
+	height="$4"
 	
 	ffmpeg -i $inputFile -vf "scale=${width}:${height}" -c:a copy $outputFile
 }
@@ -50,7 +50,7 @@ trim_video_start() {
 	
 	inputFile="$1"
 	outputFile="$2"
-	newStartTime=($3)
+	newStartTime="$3"
 	
 	ffmpeg -i $inputFile -ss $newStartTime -c:a aac $outputFile
 }
